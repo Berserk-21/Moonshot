cff19e49f2f16cea948a5d6000f0b97987186da4
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-15 11:35:20 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Berserk on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
