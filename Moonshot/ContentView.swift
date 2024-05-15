2590b9e7b266d35539dcbb755d930358a3288621
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-15 12:01:50 +0200

    Playing with scrollViews

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(.bruceLee)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
-            }
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10, content: {
+                ForEach(0..<100) {
+                    CustomText(text: "\($0)")
+                        .font(.title)
+                }
+            })
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct CustomText: View {
+    
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new CustomText")
+        self.text = text
     }
 }
 
