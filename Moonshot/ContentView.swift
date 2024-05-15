fc0dc87aa464a4ccc0229b988d627a19f9b8fea4
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-15 12:28:31 +0200

    Creates a 3 colums Grid layout

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,35 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120)),
+        GridItem(.adaptive(minimum: 80, maximum: 120)),
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
+        ScrollView {
+            LazyVGrid(columns: layout, content: {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
                 }
-            }
-            """
-
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
-            }
+            })
         }
     }
-}
-
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
 }
 
 #Preview {
