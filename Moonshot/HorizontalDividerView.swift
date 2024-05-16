283afc9bee3a93ed278639ff851efd190a310093
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-16 11:51:22 +0200

    Extract the horizontal divider in a custom view HorizontalDividerView

---
+++ Moonshot/HorizontalDividerView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalDividerView.swift
+//  Moonshot
+//
+//  Created by Berserk on 16/05/2024.
+//
+
+import SwiftUI
+
+struct HorizontalDividerView: View {
+    
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundStyle(.lightBackground)
+            .padding(.vertical)
+    }
+}
