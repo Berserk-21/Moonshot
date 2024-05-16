54572c66b41d7445cf5c1cfb060a379c97790004
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-16 10:50:11 +0200

    Adds a scroll view with nested HStack views to present astronauts of a mission

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -42,6 +42,35 @@ struct MissionView: View {
                     Text(mission.description)
                 })
                 .padding(.horizontal)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(crew, id: \.role) { crewMember in
+                            NavigationLink {
+                                Text(crewMember.astronaut.description)
+                            } label: {
+                                HStack {
+                                    Image(crewMember.astronaut.id)
+                                        .resizable()
+                                        .frame(width: 104, height: 72)
+                                        .clipShape(.capsule)
+                                        .overlay {
+                                            Capsule()
+                                                .strokeBorder(.white, lineWidth: 1)
+                                        }
+                                    VStack(alignment: .leading, content: {
+                                        Text(crewMember.astronaut.name)
+                                            .foregroundStyle(.white)
+                                            .font(.headline)
+                                        Text(crewMember.role)
+                                            .foregroundStyle(.white.opacity(0.5))
+                                    })
+                                }
+                                .padding()
+                            }
+                        }
+                    }
+                }
             }
             .padding(.bottom)
         }
